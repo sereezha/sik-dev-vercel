40e600a81db13a2ed7ae8bcf7e689db2015bb365
Serhii Hovorovskyi <hovorovskyi@gmail.com>
2024-09-14 21:30:32 +0300

    first commit

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -10,9 +10,94 @@ export const server = {
       months: z.string().min(1),
     }),
     handler: async ({ phone, telegram, bottles, months }, context) => {
-      const url = `https://sik-backend-dev.netlify.app/.netlify/functions/send?phone=${phone}&telegram=${telegram}&bottles=${bottles}&months=${months}`;
-      const response = await fetch(url);
-      return { ok: response.ok };
+      const PHONE_REGEX = /^\+?3?8?(0\d{9})$/;
+      try {
+        const tg = {
+          token: process.env.BOT_TOKEN,
+          chat_id: process.env.CHAT_ID,
+        };
+
+        if (
+          !telegram ||
+          phone.trim() === "$" ||
+          !bottles ||
+          !months ||
+          phone.match(PHONE_REGEX) === null
+        ) {
+          throw new Error("error");
+        }
+
+        const formData = new FormData();
+
+        formData.append("Phone", phone);
+        formData.append("Telegram", telegram);
+        formData.append("Bottles", bottles);
+        formData.append("Months", months);
+
+        const obj = {
+          chat_id: tg.chat_id,
+          text: `
+<b>Phone</b>: ${phone}
+${telegram ? `<b>Telegram</b>: ${telegram}` : ""}
+<b>Bottles</b>: ${bottles}
+<b>Months</b>: ${months}
+    `,
+        };
+
+        const promises = [
+          fetch(process.env.GOOGLE_SHEET as string, {
+            method: "POST",
+            body: formData,
+          }),
+          fetch(
+            `https://api.telegram.org/bot${tg.token}/sendMessage?parse_mode=html`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json; charset=UTF-8",
+              },
+              body: JSON.stringify(obj),
+            },
+          ),
+        ];
+
+        await Promise.allSettled(promises)
+          .then(() => {
+            return {
+              ok: true,
+              statusCode: 200,
+              body: JSON.stringify({ message: "success" }),
+              headers: {
+                "Access-Control-Allow-Origin": "*",
+              },
+            };
+          })
+          .catch((err) => {
+            return {
+              statusCode: 404,
+              body: err.toString(),
+            };
+          });
+
+        return {
+          ok: true,
+          statusCode: 200,
+          body: JSON.stringify({ message: "success" }),
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        };
+      } catch (e) {
+        console.log("ERROR:", e);
+        return {
+          ok: false,
+          statusCode: 500,
+          body: JSON.stringify({
+            success: false,
+            error: "Internal Server Error",
+          }),
+        };
+      }
     },
   }),
 };
