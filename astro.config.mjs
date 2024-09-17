629b297604213411dc8b9cbdbe5e73d8146698c3
Serhii Hovorovskyi <hovorovskyi@gmail.com>
2024-09-17 21:27:21 +0300

    add testing screen

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
   site: "https://hovorovskyi.com",
@@ -10,13 +9,6 @@ export default defineConfig({
 
   build: {
     inlineStylesheets: "always",
-  },
-
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true,
-    },
   },
 
   integrations: [sitemap()],
