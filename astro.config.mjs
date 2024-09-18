28f91fb764c6717c7c7e32a04ffc41f9c6166916
Serhii Hovorovskyi <hovorovskyi@gmail.com>
2024-09-18 20:37:29 +0300

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
   site: "https://hovorovskyi.com",
-  output: "server",
+  output: "hybrid",
   adapter: vercel(),
 
   build: {
     inlineStylesheets: "always",
   },
 
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
 });
