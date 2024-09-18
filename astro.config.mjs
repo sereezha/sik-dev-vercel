fd93c1876bf8fc7d7cccaad9c6451d6c5d2ffbc8
Serhii Hovorovskyi <hovorovskyi@gmail.com>
2024-09-18 09:48:47 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
-import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: "https://hovorovskyi.com",
