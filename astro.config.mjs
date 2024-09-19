51e1aa490cd1d70288f35bb976b286c0d069bd4d
Serhii Hovorovskyi <hovorovskyi@gmail.com>
2024-09-19 22:25:52 +0300

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
-
 import icon from "astro-icon";
 
 export default defineConfig({
