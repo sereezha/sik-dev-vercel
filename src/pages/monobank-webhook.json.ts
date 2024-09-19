10d3abe653486e58917220db0863c2470c3dea48
Serhii Hovorovskyi <hovorovskyi@gmail.com>
2024-09-19 10:56:17 +0300

    add prerender

--- src/pages/monobank-webhook.json.ts
+++ src/pages/monobank-webhook.json.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
 
-// export const prerender = false;
+export const prerender = false;
 
 export const GET: APIRoute = async ({ request }) => {
   return new Response(null, { status: 200 });
