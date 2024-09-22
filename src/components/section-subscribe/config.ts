17a461fa7350cb5254db1350eafdc1584340472e
Serhii Hovorovskyi <hovorovskyi@gmail.com>
2024-09-22 15:53:48 +0300

    refactoring

--- src/components/section-subscribe/config.ts
+++ src/components/section-subscribe/config.ts
@@ -1,62 +1,34 @@
+import type { SubscriptionType } from "src/services/subscriptions/types";
+
 export const bottlesMap = {
   one: 1,
   two: 2,
-  special: 'special',
+  special: "special",
 };
 
-export const benefitsMap = {
-  one: [
-    {
-      id: crypto.randomUUID(),
-      title: "Та сама таємнича пляшка",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Картка <span class='wh-nowrap'>з QR-кодом</span>, що веде <span class='wh-nowrap'>на персональне</span> відео",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Картка <span class='wh-nowrap'>з порадами</span> фудпейрингу <span class='wh-nowrap'>під ваше вино</span>",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "<span class='wh-nowrap'>та маленькі</span> приємності",
-    },
-  ],
-  two: [
-    {
-      id: crypto.randomUUID(),
-      title: "Дві пляшки вина",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Картка <span class='wh-nowrap'>з QR-кодом</span>, що веде <span class='wh-nowrap'>на персональне</span> відео",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Картка <span class='wh-nowrap'>з порадами</span> фудпейрингу <span class='wh-nowrap'>під ваше вино</span>",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "<span class='wh-nowrap'>та маленькі</span> приємності",
-    },
-  ],
-  special: [
-    {
-      id: crypto.randomUUID(),
-      title: "Одна рідкісна пляшка",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Картка <span class='wh-nowrap'>з QR-кодом</span>, що веде <span class='wh-nowrap'>на персональне</span> відео",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Картка <span class='wh-nowrap'>з порадами</span> фудпейрингу <span class='wh-nowrap'>під ваше вино</span>",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "<span class='wh-nowrap'>та маленькі</span> приємності",
-    },
-  ],
+export const benefits = [
+  {
+    id: crypto.randomUUID(),
+    title:
+      "Картка <span class='wh-nowrap'>з QR-кодом</span>, що веде <span class='wh-nowrap'>на персональне</span> відео",
+  },
+  {
+    id: crypto.randomUUID(),
+    title:
+      "Картка <span class='wh-nowrap'>з порадами</span> фудпейрингу <span class='wh-nowrap'>під ваше вино</span>",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "<span class='wh-nowrap'>та маленькі</span> приємності",
+  },
+];
+
+export const getBenefitsList = (type: SubscriptionType) => {
+  const title = {
+    one: "Та сама таємнича пляшка",
+    two: "Дві пляшки вина",
+    special: "Одна рідкісна пляшка",
+  }[type];
+
+  return [{ id: crypto.randomUUID(), title }, ...benefits];
 };
