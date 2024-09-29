b6a4bc3a2481c4f2a717813bba7fb88c0e6d1cd8
Serhii Hovorovskyi <hovorovskyi@gmail.com>
2024-09-29 12:30:14 +0300

    add icons

--- src/components/section-subscribe/config.ts
+++ src/components/section-subscribe/config.ts
@@ -1,4 +1,9 @@
 import type { SubscriptionType } from "src/services/subscriptions/types";
+import qrIcon from "@assets/img/qr.png";
+import foodPairIcon from "@assets/img/food-pair.png";
+import giftsIcon from "@assets/img/gifts.png";
+import bottle1Icon from "@assets/img/bottle-1.png";
+import bottle2Icon from "@assets/img/bottle-2.png";
 
 export const bottlesMap = {
   one: 1,
@@ -11,24 +16,33 @@ export const benefits = [
     id: crypto.randomUUID(),
     title:
       "Картка <span class='wh-nowrap'>з QR-кодом</span>, що веде <span class='wh-nowrap'>на персональне</span> відео",
+    icon: qrIcon,
   },
   {
     id: crypto.randomUUID(),
     title:
       "Картка <span class='wh-nowrap'>з порадами</span> фудпейрингу <span class='wh-nowrap'>під ваше вино</span>",
+    icon: foodPairIcon,
   },
   {
     id: crypto.randomUUID(),
     title: "<span class='wh-nowrap'>та маленькі</span> приємності",
+    icon: giftsIcon,
   },
 ];
 
 export const getBenefitsList = (type: SubscriptionType) => {
-  const title = {
-    one: "Та сама таємнича пляшка",
-    two: "Дві пляшки вина",
-    special: "Одна рідкісна пляшка",
+  const firstItem = {
+    one: {
+      title: "Та сама таємнича пляшка",
+      icon: bottle1Icon,
+    },
+    two: { title: "Дві пляшки вина", icon: bottle2Icon },
+    special: {
+      title: "Одна рідкісна пляшка",
+      icon: bottle1Icon,
+    },
   }[type];
 
-  return [{ id: crypto.randomUUID(), title }, ...benefits];
+  return [{ id: crypto.randomUUID(), ...firstItem }, ...benefits];
 };
